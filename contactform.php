7ca7506d8fc8746483cf3e6ecc5b2749e182728e
ErikRobles <erikjames69@hotmail.com>
2019-07-26 18:58:35 -0500

    changed contact form email

--- contactform.php
+++ contactform.php
@@ -5,7 +5,7 @@ if (isset($_POST['submit'])) {
     $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
 
-    $mailTo = "erikjames69@hotmail.com";
+    $mailTo = "info@rrspark.com";
     $headers = "From: " . $mailFrom;
     $txt = "You have received an e-mail from ".$name .".\n\n" .$message;
 
